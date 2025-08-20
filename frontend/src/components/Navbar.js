a0977f2b2fa854d5b079f57616bd9cbe014bd11b
Niels <nielsvdschepop12367@gmail.com>
2025-08-20 07:23:53 -0700

    .

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -54,7 +54,7 @@ function Navbar({ hideWalletConnection = false }) {
   <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
   </svg>
-  <span>Widget</span>
+  <span>Referrals</span>
 </Link>
 
 
@@ -134,7 +134,7 @@ function Navbar({ hideWalletConnection = false }) {
   <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
   </svg>
-  <span>Widget</span>
+  <span>Referrals</span>
 </Link>
 
               {/* Technical Docs Link */}
