c5837cc91e5eac7a1523627228d3e11bfbad5e25
Niels <nielsvdschepop12367@gmail.com>
2025-08-23 21:58:12 -0700

    .

--- backend/atomic-deploy-email-escrow.js
+++ backend/atomic-deploy-email-escrow.js
@@ -373,6 +373,7 @@ async function generateReclaimTransaction({ appId, senderAddress, assetId = null
       {
         txn: Buffer.from(algosdk.encodeUnsignedTransaction(closeMultisigTxn)).toString('base64'),
         authAddr: senderAddress,
+        signers: [senderAddress]
       }
     ];
     
